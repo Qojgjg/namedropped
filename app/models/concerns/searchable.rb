e8fae349c701c4b74e01753201dc0791bd6fc2ff
robertsj <rschultzejena@gmail.com>
2020-01-18 16:15:47 +0100

    Build search through episodes
    This adds a simple way to index and search through Episodes using
    Elasticsearch.
    
    The goal is to search through the title and description fields. Later we
    also want to sort results by the publication date. Because more recent episodes should be more relevant.
    
    However the sorting should boost things less than the actual relevance.
    
    ```
    sort: [ { publication_date: :asc } ]
    ```

---
+++ app/models/concerns/searchable.rb
@@ -0,0 +1,33 @@
+module Searchable
+  extend ActiveSupport::Concern
+
+  included do
+    include Elasticsearch::Model
+    include Elasticsearch::Model::Callbacks
+
+    def as_indexed_json(_options = {})
+      as_json(only: %i[title description publication_date])
+    end
+
+    settings do
+      mappings dynamic: false do
+        indexes :title, type: :text, analyzer: :english
+        indexes :description, type: :text, analyzer: :english
+        indexes :publication_date, type: :date
+      end
+    end
+
+    def self.search(query)
+      search_definition =  {
+        query: { 
+          multi_match: {
+            query: query,
+            fields: [:title, :description]
+          },
+        }
+      }
+
+      __elasticsearch__.search(search_definition)
+    end
+  end
+end
--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,4 +1,6 @@
 class Episode < ApplicationRecord
+  include Searchable
+
   belongs_to :podcast
   validates :guid, uniqueness: { scope: :podcast, message: 'The GUID has to be unique for each podcast' }
 end
