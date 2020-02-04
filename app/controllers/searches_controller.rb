18e233f57e7b93223d12ce59ed07838c34dd7621
robertsj <rschultzejena@gmail.com>
2020-02-04 10:26:49 +0100

    Have controller return search results in format for React Typeahead
    It is necessary for the library to have an id element.
    https://github.com/ericgio/react-bootstrap-typeahead/blob/master/docs/Data.md

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,9 +1,15 @@
 class SearchesController < ApplicationController
   def show
     query = params[:q]
-    # results = Podcast.where("title LIKE ?", "%#{query}%")
-    
+
+    render json: formatted_results(query).to_json
+  end
+
+  private
+
+  def formatted_results(query)
     results = Episode.search(query)
-    render json: results.to_json
+
+    formatted_results = results.each_with_index.map { |result, index| { "id" => index+1 }.merge(result.fetch("_source")) }
   end
 end
