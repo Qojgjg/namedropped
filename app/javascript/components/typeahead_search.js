f86acb35ec2600425305b733af37647c0647164a
robertsj <rschultzejena@gmail.com>
2020-02-04 10:29:39 +0100

    Fix Typeaheadsearch component
    This commit does a number of things:
    
    * It adds a search icon to be used with Bootstrap to add a search button
    and demonstrate it's a search field. The button still doesn't link to
    anything and needs to be hooked up.
    
    * Also the filterBy option is set. This was necessary to include
    description as the field the result is matched against and not just
    title. The search is already performed against Elasticsearch, so
    actually we just want to render the 10 results ES returned and not
    filter further. The example "Nick Gillespie" would return 10 results
    from Elasticsearch, but then not return results on the page, because the
    name only appeared in the description, not in the title.
    
    * This is still hardcoded to the localhost and should be passed the path
    as a prop to work in different environmnets.

--- app/javascript/components/typeahead_search.js
+++ app/javascript/components/typeahead_search.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {AsyncTypeahead} from 'react-bootstrap-typeahead';
-import GithubMenuItem from './github_menu_item';
-
-import {Menu} from 'react-bootstrap-typeahead';
-import {MenuItem} from 'react-bootstrap-typeahead';
+import EpisodeMenuItem from './episode_menu_item';
+import SearchIcon from './search_icon';
 
 class TypeaheadSearch extends React.Component {
   state = {
@@ -17,32 +15,39 @@ class TypeaheadSearch extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <AsyncTypeahead
-          isLoading={this.state.isLoading}
-          onSearch={query => {
-            this.setState({isLoading: true});
-            fetch(`http://localhost:3000/search?q=${query}`)
-              .then(resp => resp.json())
-              .then(json =>
-                this.setState({
-                  isLoading: false,
-                  options: json.items,
-                }),
-              );
-          }}
-          options={this.state.options}
-          placeholder="Search for a term in podcasts"
-          renderMenu={(results, menuProps) => (
-            <Menu {...menuProps}>
-              {results.map((result, index) => (
-                <MenuItem option={result} position={index}>
-                  {result}
-                </MenuItem>
-              ))}
-            </Menu>
-          )}
-          labelKey={option => `${option._id}`}
-        />
+        <div className="input-group mb-3">
+          <AsyncTypeahead
+            isLoading={this.state.isLoading}
+            onSearch={query => {
+              this.setState({isLoading: true});
+              fetch(`http://localhost:3000/search?q=${query}`)
+                .then(resp => resp.json())
+                .then(json =>
+                  this.setState({
+                    isLoading: false,
+                    options: json,
+                  }),
+                );
+            }}
+            options={this.state.options}
+            placeholder="Search for a term in podcasts"
+            renderMenuItemChildren={(option, props) => (
+              <EpisodeMenuItem key={option.id} episode={option} />
+            )}
+            labelKey={option => `${option.title}`}
+            filterBy={['title', 'description']}
+          />
+        {console.log(this.state.options)}
+          <div className="input-group-append">
+            <button
+              className="btn btn-outline-secondary"
+              type="button"
+              id="button-addon2"
+            >
+              <SearchIcon />
+            </button>
+          </div>
+        </div>
       </React.Fragment>
     );
   }
