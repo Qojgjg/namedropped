8ac2d215da66e869e8eec73fa9df1deef28db249
robertsj <rschultzejena@gmail.com>
2020-02-18 19:36:30 +0100

    Render search results
    This adds a few commits to render search results upon clicking on the
    search button - the magnifying glass.
    
    It currently works with the results from the request made by Typeahead
    React Bootstrap. This might have to be revisited later.
    
    The search result layout comes from
    https://getbootstrap.com/docs/4.4/examples/offcanvas/

--- app/javascript/components/typeahead_search.js
+++ app/javascript/components/typeahead_search.js
@@ -1,33 +1,51 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import {AsyncTypeahead} from 'react-bootstrap-typeahead';
-import EpisodeMenuItem from './episode_menu_item';
-import SearchIcon from './search_icon';
+import React from "react";
+import PropTypes from "prop-types";
+import { AsyncTypeahead } from "react-bootstrap-typeahead";
+import EpisodeMenuItem from "./episode_menu_item";
+import SearchIcon from "./search_icon";
+import SearchResults from "./search_results";
 
 class TypeaheadSearch extends React.Component {
-  state = {
-    allowNew: false,
-    isLoading: false,
-    multiple: false,
-    options: [],
-  };
+  constructor(props) {
+    super(props);
+    this.handleSearchClick = this.handleSearchClick.bind(this);
+
+    this.state = {
+      allowNew: false,
+      isLoading: false,
+      multiple: false,
+      options: [],
+      searchQuery: "",
+      searchClick: false
+    };
+  }
+
+  handleSearchClick() {
+    this.setState(state => ({
+      searchClick: true
+    }));
+  }
 
   render() {
+    const { searchClick = false } = this.state;
+
     return (
       <React.Fragment>
         <div className="input-group mb-3">
           <AsyncTypeahead
             isLoading={this.state.isLoading}
             onSearch={query => {
-              this.setState({isLoading: true});
+              this.setState({ isLoading: true });
+              this.setState({ searchClick: false });
               fetch(`${this.props.searchPath}?q=${query}`)
                 .then(resp => resp.json())
                 .then(json =>
                   this.setState({
                     isLoading: false,
-                    options: json,
-                  }),
+                    options: json
+                  })
                 );
+              this.state.searchQuery = query;
             }}
             options={this.state.options}
             placeholder="Search through podcast episodes..."
@@ -35,19 +53,28 @@ class TypeaheadSearch extends React.Component {
               <EpisodeMenuItem key={option.id} episode={option} />
             )}
             labelKey={option => `${option.title}`}
-            filterBy={['title', 'description']}
+            filterBy={["title", "description"]}
+            ref={typeahead => (this.typeahead = typeahead)}
           />
-        {console.log(this.state.options)}
+          {console.log(this.state.options)}
           <div className="input-group-append">
             <button
               className="btn btn-outline-secondary"
               type="button"
               id="button-addon2"
+              onClick={this.handleSearchClick}
             >
               <SearchIcon />
             </button>
           </div>
         </div>
+
+        {searchClick && (
+          <SearchResults
+            results={this.state.options}
+            searchQuery={this.state.searchQuery}
+          />
+        )}
       </React.Fragment>
     );
   }
@@ -56,7 +83,7 @@ class TypeaheadSearch extends React.Component {
 TypeaheadSearch.propTypes = {
   isLoading: PropTypes.bool,
   onSearch: PropTypes.string,
-  searchPath: PropTypes.string.isRequired,
+  searchPath: PropTypes.string.isRequired
 };
 
 export default TypeaheadSearch;
