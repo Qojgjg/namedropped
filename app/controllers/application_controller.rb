39eb7e40148efd159e94fddfa2f0f197b48b38f4
robertsj <rschultzejena@gmail.com>
2020-02-19 21:15:09 +0100

    Only require basic auth in production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with name: ENV['BASIC_AUTH_USER'], password: ENV['BASIC_AUTH_PASSWORD']
+  if Rails.env == 'production'
+    http_basic_authenticate_with name: ENV['BASIC_AUTH_USER'], password: ENV['BASIC_AUTH_PASSWORD']
+  end
 end
