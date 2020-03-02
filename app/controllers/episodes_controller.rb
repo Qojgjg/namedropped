a8faa10e641d33502e47dd6af5a7873bba23fdc1
robertsj <rschultzejena@gmail.com>
2020-03-02 10:04:40 +0100

    Track Searches and showing of episodes with Ahoy from the controller

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,7 +1,15 @@
 class EpisodesController < ApplicationController
+  after_action :track_action, only: :show
+
   def show
     @episode = Episode.find(params[:id])
     @podcast = @episode.podcast
     @search_term = cookies.encrypted[:search_term]
+  end
+
+  private
+
+  def track_action
+    ahoy.track "Show Episode", request.path_parameters.merge(search_term: @search_term)
   end
 end
--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,16 +1,22 @@
 class SearchesController < ApplicationController
-  def show
-    query = params[:q]
-    cookies.encrypted[:search_term] = query
+  after_action :track_action, only: :show
 
-    render json: formatted_results(query).to_json
+  def show
+    @query = params[:q]
+    cookies.encrypted[:search_term] = @query
+
+    render json: formatted_results.to_json
   end
 
   private
 
-  def formatted_results(query)
-    results = Episode.search(query)
+  def formatted_results
+    results = Episode.search(@query)
 
     formatted_results = results.each_with_index.map { |result, index| { "id" => index+1 }.merge(result.fetch("_source")) }
+  end
+
+  def track_action
+    ahoy.track "Search", request.path_parameters.merge(search_term: @query)
   end
 end
