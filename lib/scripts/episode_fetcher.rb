e42adb52fe4b951079ec48e56cc02f5152c1ddce
robertsj <rschultzejena@gmail.com>
2020-03-02 10:29:06 +0100

    Fix Episode fetcher script

--- lib/scripts/episode_fetcher.rb
+++ lib/scripts/episode_fetcher.rb
@@ -1,8 +1,10 @@
+require_relative 'lib/crawler/podcast_crawler'
+
 module Searchable; end
 
-Podcast.each_find do |podcast|
+Podcast.find_each do |podcast|
   crawler = Crawler::PodcastCrawler.new(podcast)
   crawler.update_podcast_episodes_info
 end
 
-# run with bundle exec rails runner > output.txt
+# run with bundle exec rails runner lib/scripts/episode_fetcher.rb > output.txt
