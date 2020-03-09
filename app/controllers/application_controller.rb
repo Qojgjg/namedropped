21a60694d71c2cb87b60d8f0398e57940d7460a4
robertsj <rschultzejena@gmail.com>
2020-03-09 22:46:47 +0100

    Protect from forgery, with exception
    If CSRF is not verified, it fails as it did before. But this time with
    an exception.
    
    This guards against accidentally executing code in the future despite an
    invalid CSRF token.
    
    See
    https://nvisium.com/blog/2014/09/10/understanding-protectfromforgery.html
    and the official docs:
    
    https://api.rubyonrails.org/classes/ActionController/RequestForgeryProtection/ClassMethods.html#method-i-protect_from_forgery
    
    It also makes a test in the searches controller spec easier to write.
    Now, with the wrong CSRF token and `protect_from_forgery` disabled, an
    `InvalidAuthenticityToken` is raised.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  protect_from_forgery with: :exception
 
   if Rails.env == 'production'
     http_basic_authenticate_with name: ENV['BASIC_AUTH_USER'], password: ENV['BASIC_AUTH_PASSWORD']
