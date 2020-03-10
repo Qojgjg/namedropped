7eaacb895d9635f66b47ab0306fac30616724740
robertsj <rschultzejena@gmail.com>
2020-03-10 21:28:14 +0100

    Install Sidekiq
    This installs Sidekiq. Redis is automatically installed as well as the
    datastore where jobs are enqueued.
    
    It also adds a admin route to view sidekiq stats under /admin/sidekiq
    
    Note that this route should be locked down with devise and is currently
    publicly available. However production is still behind basic
    authentication.
    
    https://github.com/mperham/sidekiq/wiki/Getting-Started

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/admin/sidekiq'
+
   root to: 'homepage#index'
 
   get '/typeahead_search', to: 'searches#typeahead_search'
