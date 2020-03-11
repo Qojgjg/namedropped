d60e52e261fdc703adbfa0c6b5738cf1b090287a
robertsj <rschultzejena@gmail.com>
2020-03-11 10:28:56 +0100

    Simplify episode fetcher script to trigger the episode fetch worker

--- lib/scripts/episode_fetcher.rb
+++ lib/scripts/episode_fetcher.rb
@@ -1,10 +1,3 @@
-require_relative 'lib/crawler/podcast_crawler'
-
-module Searchable; end
-
 Podcast.find_each do |podcast|
-  crawler = Crawler::PodcastCrawler.new(podcast)
-  crawler.update_podcast_episodes_info
+  EpisodeFetchWorker.perform_async(podcast.id)
 end
-
-# run with bundle exec rails runner lib/scripts/episode_fetcher.rb > output.txt
