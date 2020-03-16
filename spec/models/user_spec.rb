8bdfa95ff31d4c3acc14e921a6b456ff204a1fbf
robertsj <rschultzejena@gmail.com>
2020-03-16 21:56:00 +0100

    Validate presence of email for users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'validates an email address is present' do
+    user = User.create
+    expect(user).not_to be_valid
+  end
 end
