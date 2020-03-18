7ba12176aa08a4d12ffb19dcf59f2643655a1328
robertsj <rschultzejena@gmail.com>
2020-03-18 22:48:24 +0100

    Make password not required for users
    Initially it should be possible to enter an email address and a search
    term, without having to go another step and create an account. So
    creating a user record should be possible without a password.
    
    In this guide it only follos one step:
    https://github.com/heartcombo/devise/wiki/How-To:-Email-only-sign-up

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :email, presence: true
+
+  protected
+
+  def password_required?
+    false
+  end
 end
