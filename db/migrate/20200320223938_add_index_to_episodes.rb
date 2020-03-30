03401fda88b81d79c7f4964720c58a35fe43332a
robertsj <rschultzejena@gmail.com>
2020-03-30 22:25:03 +0200

    Remove reverted migrations
    These migrations had been reverted in commit bbc05c380d, but the files
    weren't removed. That means that `rails db:migrate` would run them
    again.
    
    Now that they are deleted, they should not reappear.

--- db/migrate/20200320223938_add_index_to_episodes.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToEpisodes < ActiveRecord::Migration[6.0]
-  def change
-    add_index :episodes, :id
-  end
-end
--- db/migrate/20200320224704_add_guid_index_to_episodes.rb
+++
@@ -1,5 +0,0 @@
-class AddGuidIndexToEpisodes < ActiveRecord::Migration[6.0]
-  def change
-    add_index :episodes, :guid
-  end
-end
