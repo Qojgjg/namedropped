dcad98dd3088d9e59c31cc69e234007c90337108
robertsj <rschultzejena@gmail.com>
2020-04-05 16:44:43 +0200

    Sequence unique data in podcasts and users
    itunes_id in podcasts and email in users should be unique. FactoryBot's
    sequencing can help create unique records, so if multiple records are
    created in a test, no error is raised because of uniqueness constraints.

--- spec/factories/podcast.rb
+++ spec/factories/podcast.rb
@@ -1,6 +1,9 @@
 FactoryBot.define do
+  sequence :itunes_id
+
   factory :podcast do
     title { "Reason Editors Roundtable" }
     rss  { "rss-feed-address.com" }
+    itunes_id { FactoryBot.generate(:itunes_id) }
   end
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
+  sequence :user_email do |n|
+    "person#{n}@example.com"
+  end
+
   factory :user do
-    
+    email { FactoryBot.generate(:user_email) }
   end
 end
