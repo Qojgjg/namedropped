be6974d555447c1aa4d8c57d6c7cdf45f58f0030
robertsj <rschultzejena@gmail.com>
2020-04-07 22:41:08 +0200

    Add feature spec to test crawling of episodes for search_term
    This feature spec mirrors the app's behavior, without user involvement.
    It traces the steps of
    1.) The cronjob executes a rake task
    2.) Rake task goes through search_terms and passes each to a worker
    3.) The worker calls the SearchTermCrawlerService
    4.) The Service uses Elasticsearch to search through all episodes for
        the search term
        Any matches are stored in the search_term_matches table
    
    We need to wait for Sidekiq after the job has been enqueued before
    draining the queue and thereby executing the job.
    
    Unfortunately at this point the cronjob and the rake task are not used
    in this feature spec, but the code that the rake task calls is executed
    directly in the spec.

---
+++ spec/features/crawling_episodes_for_search_term_matches_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.feature 'crawling episodes for a search_term_match', elasticsearch: true do
+  scenario 'crawling epispdes for a search_term_match' do
+    given_one_matching_episode_exists
+    when_a_user_has_a_search_term
+    and_the_cron_job_runs_the_rake_task
+    then_the_sidekiq_worker_runs_the_job
+    and_the_matches_are_stored
+  end
+
+  private
+
+  def when_a_user_has_a_search_term
+    FactoryBot.create(:search_term, name: 'Ruby')
+  end
+
+  def and_the_cron_job_runs_the_rake_task
+    SearchTerm.find_each do |search_term|
+      SearchTermCrawlerWorker.perform_async(search_term.id)
+    end
+  end
+
+  def then_the_sidekiq_worker_runs_the_job
+    wait_for_sidekiq
+    SearchTermCrawlerWorker.drain
+  end
+
+  def and_the_matches_are_stored
+    expect(SearchTermMatch.count).to eq(1)
+    expect(SearchTermMatch.last.episode.title).to eq('Fun with Ruby on Rails')
+  end
+
+  def given_one_matching_episode_exists
+    FactoryBot.create(:episode, title: 'Fun with Ruby on Rails')
+    FactoryBot.create(:episode, title: 'Fun with Python')
+  end
+
+  def wait_for_sidekiq
+    sleep 1
+  end
+end
+
+
