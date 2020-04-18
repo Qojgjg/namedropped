6fa80868f1457adcdc9f2225a47b06a605327ef0
robertsj <rschultzejena@gmail.com>
2020-04-18 23:58:22 +0200

    Change feature spec to adjust to emailing only once about multiple search_term_matches
    The indexing in Elasticsearch was created in 36594d6
    and justified by episodes being created as a secondary effect of
    creating search_term_matches. But the existence of an episode does not
    mean it has to be indexed in Elasticsearch. And since Elasticsearch
    functioanlity is not necessary for this feature spec, we can remove the
    elasticsearch flag from the spec and rely on the stubbing of indexing
    documents in Elasticsearch via the spec_helper before blocks.

--- spec/features/notifying_users_of_search_term_matches_spec.rb
+++ spec/features/notifying_users_of_search_term_matches_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.feature 'notifying users of search_term_matches', elasticsearch: true do
+RSpec.feature 'notifying users of search_term_matches' do
   scenario 'notifying users of search_term_matches' do
-    when_a_search_term_has_been_found_in_an_episode
+    when_a_search_term_has_been_found_in_two_episodes
     and_the_cron_job_runs_the_rake_task
     when_the_sidekiq_worker_runs_the_job
     then_the_user_is_notified_via_email
@@ -11,15 +11,15 @@ RSpec.feature 'notifying users of search_term_matches', elasticsearch: true do
 
   private
 
-  def when_a_search_term_has_been_found_in_an_episode
-    @search_term = FactoryBot.create(:search_term, created_at: 1.day.ago)
-    @search_term_match = FactoryBot.create(:search_term_match, search_term: @search_term, user: @search_term.user)
+  def when_a_search_term_has_been_found_in_two_episodes
+    @user = FactoryBot.create(:user)
+    @search_term = FactoryBot.create(:search_term, created_at: 1.day.ago, user: @user)
+    @search_term_match = FactoryBot.create(:search_term_match, search_term: @search_term, user: @user)
+    @search_term_match = FactoryBot.create(:search_term_match, search_term: @search_term, user: @user)
   end
 
   def and_the_cron_job_runs_the_rake_task
-    SearchTermMatch.due_for_notification.find_each do |search_term_match|
-      SearchTermMatchNotifierWorker.perform_async(search_term_match.id)
-    end
+    SearchTermMatchNotifierWorker.perform_async(SearchTermMatch.due_for_notification.pluck(:user_id))
   end
 
   def when_the_sidekiq_worker_runs_the_job
@@ -29,9 +29,8 @@ RSpec.feature 'notifying users of search_term_matches', elasticsearch: true do
 
   def then_the_user_is_notified_via_email
     emails_received = ActionMailer::Base.deliveries
-    user = @search_term.user
     expect(emails_received.last.subject).to eq('Tuple pairing was namedropped on a podcast')
-    expect(emails_received.last.to).to eq([user.email])
+    expect(emails_received.last.to).to eq([@user.email])
   end
 
   def and_the_notification_is_recorded
