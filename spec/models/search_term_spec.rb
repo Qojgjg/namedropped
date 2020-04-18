85eff42ec4b7bccf9927c88a43e26ec90a57a88e
robertsj <rschultzejena@gmail.com>
2020-04-18 10:00:55 +0200

    Create SearchTerm.with_matches_due_for_notification scope
    This should return all search_terms, that have search_term_matches due
    for notification.
    
    When rewriting the notification mailer to include all
    search_term_matches in one email rather than send one email per
    search_term_match, it became apparent that we wanted to query from user
    to search_term to search_term_match. This scope should make it easier to
    get the relevant search_terms with its search_term_matches.

---
+++ spec/models/search_term_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'SearchTerm', type: :model do
+  before do
+    stub_request(:put, "http://localhost:9250/episodes/_doc/1")
+  end
+
+  describe '.with_matches_due_for_notification' do
+    let(:search_term_one) { FactoryBot.create(:search_term, created_at: 1.day.ago) }
+    let(:search_term_two) { FactorBot.create(:search_term) }
+
+    before do
+      FactoryBot.create(:search_term_match, search_term: search_term_one)
+    end
+
+    it 'returns search_terms that were created before today where the search_term_match has not been notified' do
+      search_terms = SearchTerm.with_matches_due_for_notification
+      expect(search_terms).to eq([search_term_one])
+    end
+  end
+end
