62f5b93d90510a6c0b17b309fda834e2b8b9b2f5
robertsj <rschultzejena@gmail.com>
2020-04-18 22:32:35 +0200

    Change SearchTermMatchNotifierService to handle one email for multiple search_term_matches
    This refactors the service, so a user can receive one email with
    multiple search terms.
    
    The service receives a list of user_ids, as it will go through Sidekiq
    which can only receive plain ruby objects. From the ids the users are
    found and for each user the search_terms are retrieved, that have
    search_term_matches that the user has not been notified about yet.
    
    The queries run from user -> search_term -> search_term_match

--- spec/services/search_term_match_notifier_service_spec.rb
+++ spec/services/search_term_match_notifier_service_spec.rb
@@ -1,22 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe SearchTermMatchNotifierService do
-  let(:search_term_match) { double(:search_term_match) }
-  let(:subject) { described_class.new(search_term_match) }
+  let(:users_with_search_term_match_ids) { [user_one.id, user_two.id, user_three.id, user_four.id] }
+
+  let(:user_one) { FactoryBot.create(:user) }
+  let(:user_two) { FactoryBot.create(:user) }
+  let(:user_three) { FactoryBot.create(:user) }
+  let(:user_four) { FactoryBot.create(:user) }
+
+  let(:search_term_one) { FactoryBot.create(:search_term, user: user_one, created_at: 1.day.ago) }
+  let(:search_term_two) { FactoryBot.create(:search_term, user: user_two, created_at: 1.day.ago) }
+
+  let(:subject) { described_class.new(users_with_search_term_match_ids) }
+  let(:mail) { double(:mail, deliver_later: nil) }
 
   before do
-    allow(search_term_match).to receive(:update)
-    allow(NotificationMailer).to receive_message_chain(:notify_user_of_search_term_match, :deliver_later)
+    allow(NotificationMailer).to receive(:notify_user_of_search_term_matches).and_return(mail)
   end
+
+  let!(:search_term_match_one) { FactoryBot.create(:search_term_match, user: user_one, search_term: search_term_one) }
+  let!(:search_term_match_two) { FactoryBot.create(:search_term_match, user: user_two, search_term: search_term_two) }
 
   describe '#notify' do
     it 'sends the email' do
-      expect(NotificationMailer).to receive_message_chain(:notify_user_of_search_term_match, :deliver_later)
+      expect(NotificationMailer).to receive(:notify_user_of_search_term_matches).with(user_one, search_term_one, search_term_one.search_term_matches)
       subject.notify
     end
 
-    it 'creates a notification record' do
-      expect(search_term_match).to receive(:update).with(notified_on: DateTime.now)  
+    it 'marks the search_term_matches as notified' do
+      subject.notify
+      expect(search_term_match_one.reload.notified_on.to_date.to_s).to eq(Date.current.to_s)
+    end
+
+    it 'sends two emails' do
+      expect(NotificationMailer).to receive(:notify_user_of_search_term_matches).twice
+      subject.notify
+    end
+
+    it 'sends one email to each user with search_term_matches' do
+      expect(NotificationMailer).to receive(:notify_user_of_search_term_matches).with(user_one, search_term_one, search_term_one.search_term_matches)
+      expect(NotificationMailer).to receive(:notify_user_of_search_term_matches).with(user_two, search_term_two, search_term_two.search_term_matches)
       subject.notify
     end
   end
