6f01107524d18e10b7c50f760b163ffd1a24703e
robertsj <rschultzejena@gmail.com>
2020-04-18 23:01:23 +0200

    Change NotificationMailer
    It can now handle multiple search_term_matches in one email.

--- spec/mailers/notification_mailer_spec.rb
+++ spec/mailers/notification_mailer_spec.rb
@@ -1,12 +1,17 @@
 require "rails_helper"
 
 RSpec.describe NotificationMailer, type: :mailer do
-  let(:mail) { NotificationMailer.notify_user_of_search_term_match(search_term_match) }
-  let(:search_term_match) { double(:search_term_match, user: user, search_term: search_term, episode: episode) }
+  let(:mail) { NotificationMailer.notify_user_of_search_term_matches(user, search_term, search_term_matches) }
+  let(:search_term_match_one) { double(:search_term_match_one, user: user, search_term: search_term, episode: episode_one) }
+  let(:search_term_match_two) { double(:search_term_match_one, user: user, search_term: search_term, episode: episode_two) }
+  let(:search_term_matches) { [search_term_match_one, search_term_match_two] }
 
   let(:user) { double(:user, email: 'email@domain.com') }
   let(:search_term) { double(:search_term, name: 'Balaji Srinivasan') }
-  let(:episode) { instance_double('Episode', :episode, title: 'Podcast episode title') }
+  let(:episode_one) { instance_double('Episode', :episode, title: 'Podcast episode title one', podcast: podcast_one) }
+  let(:episode_two) { instance_double('Episode', :episode, title: 'Podcast episode title two', podcast: podcast_two) }
+  let(:podcast_one) { instance_double('Podcast', :podcast, title: 'Podcast One') }
+  let(:podcast_two) { instance_double('Podcast', :podcast, title: 'Podcast Two') }
 
   it 'sends email from notification@namedropped.co' do
     expect(mail.from).to include('notification@namedropped.co')
@@ -18,5 +23,10 @@ RSpec.describe NotificationMailer, type: :mailer do
 
   it 'sends the email to the user' do
     expect(mail.to).to include(user.email)
+  end
+
+  it 'mentions both matches in the body of the email' do
+    expect(mail.body.encoded).to match("Podcast One")
+    expect(mail.body.encoded).to match("Podcast Two")
   end
 end
--- spec/mailers/previews/notification_mailer_preview.rb
+++ spec/mailers/previews/notification_mailer_preview.rb
@@ -1,8 +1,10 @@
 # Preview all emails at http://localhost:3000/rails/mailers/episode_alert_mailer
 class NotificationMailerPreview < ActionMailer::Preview
-  def notify_user_of_search_term_match
-    search_term_match = SearchTermMatch.last
-    NotificationMailer.notify_user_of_search_term_match(search_term_match)
+  def notify_user_of_search_term_matches
+    search_term_matches = SearchTermMatch.last(10)
+    search_term = search_term_matches.last.search_term
+    user = User.last
+    NotificationMailer.notify_user_of_search_term_matches(user, search_term, search_term_matches)
   end
 end
 
